06a2d13a3a4d92e8f1dfc738d10fe3e21dc40289
Yassine6262 <yassineaz2018@outlook.com>
2024-10-16 11:38:47 +0200

    home

---
+++ partials/nav.php
@@ -0,0 +1,11 @@
+<nav>
+    <div class="logo">Elektronisch Stemmen</div>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="#">Hoe het Werkt</a></li>
+        <li><a href="#">Veiligheid</a></li>
+        <li><a href="#">FAQ</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">NL/EN</a></li>
+    </ul>
+</nav>
