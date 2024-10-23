b7ca9abf79e0a2f855a720e1ee622a68a2f51b08
Bilal200614 <9019098@student.zadkine.nl>
2024-10-23 12:09:54 +0200

    commit

--- includes/header.php
+++ includes/header.php
@@ -4,14 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Stem Systeem</title>
-    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
 </head>
 <body>
     <header>
         <h1>Stem Systeem</h1>
         <nav>
             <ul>
-                <li><a href="/index.php">Home</a></li>
+                <li><a href="../index.php">Home</a></li>
                 <li><a href="/logout.php">Uitloggen</a></li>
             </ul>
         </nav>
